87fc3e0e0ccf99c2aa536c6ffa51b1c6590dd6f1
Nevinson <info@nevinson.co.za>
2017-08-06 00:39:26 +0200

    racing events management form created....

---
+++ RoadWarriors/frmRacingEvents.Designer.vb
@@ -0,0 +1,424 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRacingEvents
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoadAllEventsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ResetFieldsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RacingDriversToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RacingEventsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EventsPerfomanceResultsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewHelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtNumberofLaps = New System.Windows.Forms.TextBox()
+        Me.txtEventLocation = New System.Windows.Forms.TextBox()
+        Me.txtRegistrationFee = New System.Windows.Forms.TextBox()
+        Me.dteDateofEvent = New System.Windows.Forms.DateTimePicker()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dgvRacingEvents = New System.Windows.Forms.DataGridView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgvRacingEvents, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.ViewToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(881, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadAllEventsToolStripMenuItem, Me.ResetFieldsToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'LoadAllEventsToolStripMenuItem
+        '
+        Me.LoadAllEventsToolStripMenuItem.Name = "LoadAllEventsToolStripMenuItem"
+        Me.LoadAllEventsToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.LoadAllEventsToolStripMenuItem.Text = "L&oad All Events"
+        '
+        'ResetFieldsToolStripMenuItem
+        '
+        Me.ResetFieldsToolStripMenuItem.Name = "ResetFieldsToolStripMenuItem"
+        Me.ResetFieldsToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.ResetFieldsToolStripMenuItem.Text = "Reset &Fields"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(154, 22)
+        Me.ExitToolStripMenuItem.Text = "&Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateToolStripMenuItem, Me.UpdateToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edi&t"
+        '
+        'CreateToolStripMenuItem
+        '
+        Me.CreateToolStripMenuItem.Name = "CreateToolStripMenuItem"
+        Me.CreateToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.CreateToolStripMenuItem.Text = "&Create"
+        '
+        'UpdateToolStripMenuItem
+        '
+        Me.UpdateToolStripMenuItem.Name = "UpdateToolStripMenuItem"
+        Me.UpdateToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.UpdateToolStripMenuItem.Text = "&Update"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.DeleteToolStripMenuItem.Text = "&Delete"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RacingDriversToolStripMenuItem, Me.RacingEventsToolStripMenuItem, Me.EventsPerfomanceResultsToolStripMenuItem})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "&View"
+        '
+        'RacingDriversToolStripMenuItem
+        '
+        Me.RacingDriversToolStripMenuItem.Name = "RacingDriversToolStripMenuItem"
+        Me.RacingDriversToolStripMenuItem.Size = New System.Drawing.Size(215, 22)
+        Me.RacingDriversToolStripMenuItem.Text = "&Racing Drivers"
+        '
+        'RacingEventsToolStripMenuItem
+        '
+        Me.RacingEventsToolStripMenuItem.Enabled = False
+        Me.RacingEventsToolStripMenuItem.Name = "RacingEventsToolStripMenuItem"
+        Me.RacingEventsToolStripMenuItem.Size = New System.Drawing.Size(215, 22)
+        Me.RacingEventsToolStripMenuItem.Text = "Racing Eve&nts"
+        '
+        'EventsPerfomanceResultsToolStripMenuItem
+        '
+        Me.EventsPerfomanceResultsToolStripMenuItem.Name = "EventsPerfomanceResultsToolStripMenuItem"
+        Me.EventsPerfomanceResultsToolStripMenuItem.Size = New System.Drawing.Size(215, 22)
+        Me.EventsPerfomanceResultsToolStripMenuItem.Text = "Events &Perfomance Results"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewHelpToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'ViewHelpToolStripMenuItem
+        '
+        Me.ViewHelpToolStripMenuItem.Name = "ViewHelpToolStripMenuItem"
+        Me.ViewHelpToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.ViewHelpToolStripMenuItem.Text = "View He&lp"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(127, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnSearch)
+        Me.GroupBox1.Controls.Add(Me.txtSearch)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(303, 77)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search for Racing Event"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Enabled = False
+        Me.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnSearch.Location = New System.Drawing.Point(179, 47)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(87, 24)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "&Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(146, 20)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(151, 20)
+        Me.txtSearch.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Search:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtNumberofLaps)
+        Me.GroupBox2.Controls.Add(Me.txtEventLocation)
+        Me.GroupBox2.Controls.Add(Me.txtRegistrationFee)
+        Me.GroupBox2.Controls.Add(Me.dteDateofEvent)
+        Me.GroupBox2.Controls.Add(Me.txtTitle)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 111)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(303, 173)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Racing Event Details"
+        '
+        'txtNumberofLaps
+        '
+        Me.txtNumberofLaps.Location = New System.Drawing.Point(146, 136)
+        Me.txtNumberofLaps.Name = "txtNumberofLaps"
+        Me.txtNumberofLaps.Size = New System.Drawing.Size(151, 20)
+        Me.txtNumberofLaps.TabIndex = 9
+        '
+        'txtEventLocation
+        '
+        Me.txtEventLocation.Location = New System.Drawing.Point(146, 107)
+        Me.txtEventLocation.Name = "txtEventLocation"
+        Me.txtEventLocation.Size = New System.Drawing.Size(151, 20)
+        Me.txtEventLocation.TabIndex = 8
+        '
+        'txtRegistrationFee
+        '
+        Me.txtRegistrationFee.Location = New System.Drawing.Point(146, 78)
+        Me.txtRegistrationFee.Name = "txtRegistrationFee"
+        Me.txtRegistrationFee.Size = New System.Drawing.Size(151, 20)
+        Me.txtRegistrationFee.TabIndex = 7
+        '
+        'dteDateofEvent
+        '
+        Me.dteDateofEvent.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dteDateofEvent.Location = New System.Drawing.Point(146, 49)
+        Me.dteDateofEvent.Name = "dteDateofEvent"
+        Me.dteDateofEvent.Size = New System.Drawing.Size(151, 20)
+        Me.dteDateofEvent.TabIndex = 6
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Location = New System.Drawing.Point(146, 20)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(151, 20)
+        Me.txtTitle.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 143)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Number of Laps:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 114)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Event Location:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(87, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Registration Fee:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Date of Event:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(85, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Title:"
+        '
+        'dgvRacingEvents
+        '
+        Me.dgvRacingEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvRacingEvents.Location = New System.Drawing.Point(335, 113)
+        Me.dgvRacingEvents.Name = "dgvRacingEvents"
+        Me.dgvRacingEvents.Size = New System.Drawing.Size(534, 171)
+        Me.dgvRacingEvents.TabIndex = 8
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Red
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExit.Location = New System.Drawing.Point(773, 290)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(87, 24)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Enabled = False
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDelete.Location = New System.Drawing.Point(248, 290)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(87, 24)
+        Me.btnDelete.TabIndex = 11
+        Me.btnDelete.Text = "&Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Enabled = False
+        Me.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnUpdate.Location = New System.Drawing.Point(128, 290)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(87, 24)
+        Me.btnUpdate.TabIndex = 10
+        Me.btnUpdate.Text = "&Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnCreate.Location = New System.Drawing.Point(12, 290)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(87, 24)
+        Me.btnCreate.TabIndex = 9
+        Me.btnCreate.Text = "&Create "
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'frmRacingEvents
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(881, 326)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.dgvRacingEvents)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "frmRacingEvents"
+        Me.Text = "frmRacingEvents"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.dgvRacingEvents, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoadAllEventsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetFieldsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UpdateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RacingDriversToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RacingEventsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EventsPerfomanceResultsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewHelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtNumberofLaps As TextBox
+    Friend WithEvents txtEventLocation As TextBox
+    Friend WithEvents txtRegistrationFee As TextBox
+    Friend WithEvents dteDateofEvent As DateTimePicker
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dgvRacingEvents As DataGridView
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnCreate As Button
+End Class
---
+++ RoadWarriors/frmRacingEvents.vb
@@ -0,0 +1,110 @@
+﻿Public Class frmRacingEvents
+
+#Region "Main Controls"
+    Private Sub frmRacingEvents_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''
+
+    End Sub
+
+
+    Private Sub dgvRacingEvents_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRacingEvents.CellContentClick
+        ''
+
+    End Sub
+
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+        ''
+
+    End Sub
+
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        ''
+
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        ''
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        ''
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        ''
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ''
+
+    End Sub
+#End Region
+
+#Region "File Menu Strip"
+    Private Sub LoadAllEventsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadAllEventsToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub ResetFieldsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ResetFieldsToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        ''
+
+    End Sub
+#End Region
+
+#Region "Edit Menu Strip"
+    Private Sub CreateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub UpdateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UpdateToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
+        ''
+
+    End Sub
+#End Region
+
+#Region "View Menu Strip"
+    Private Sub RacingDriversToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RacingDriversToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub RacingEventsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RacingEventsToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub EventsPerfomanceResultsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EventsPerfomanceResultsToolStripMenuItem.Click
+        ''
+
+    End Sub
+#End Region
+
+#Region "Help Menu Strip"
+    Private Sub ViewHelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ViewHelpToolStripMenuItem.Click
+        ''
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        ''
+
+    End Sub
+#End Region
+End Class
