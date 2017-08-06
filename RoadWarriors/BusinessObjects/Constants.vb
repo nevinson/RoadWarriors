ef8d0950b6600f10e8632c1530405b29a977336f
Nevinson <info@nevinson.co.za>
2017-08-06 23:39:25 +0200

    constants class created.... connection string and files location methods created

---
+++ RoadWarriors/BusinessObjects/Constants.vb
@@ -0,0 +1,22 @@
+﻿Public Class Constants
+
+    Public Function RacingDriverFileLocation() As String
+        ''
+        Return Application.StartupPath & "/DataStorage/RacingDrivers.csv"
+    End Function
+
+    Public Function RacingEventsFileLocation() As String
+        ''
+        Return Application.StartupPath & "/DataStorage/RacingEvents.csv"
+    End Function
+
+    Public Function EventsResultsFileLocation() As String
+        ''
+        Return Application.StartupPath & "/DataStorage/EventsResults.csv"
+    End Function
+
+    Public Function ConnectionString() As String
+        ''
+        Return "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "/DataStorage;Extended Properties='text;HDR=Yes;FMT=Delimited;CharacterSet=65001;'"
+    End Function
+End Class
